238f49cb3a5542f7e46c973874b2f3d0903a6d4e
AJOsmaston <dajoxx@gmail.com>
2021-11-24 17:30:36 +0000

    max quality never more than 50

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -9,25 +9,49 @@ describe GildedRose do
     describe "all items" do
 
       before(:each) do
-        @items_qual_0 = [
-          Item.new(name="+5 Dexterity Vest", sell_in=10, quality=0),
-          Item.new(name="Aged Brie", sell_in=2, quality=0),
-          Item.new(name="Elixir of the Mongoose", sell_in=5, quality=0),
-          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=0),
-          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=0),
-          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=0),
-          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=0),
-          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=0),
+        min_quality = GildedRose::MIN_QUALITY
+        max_quality = GildedRose::MAX_QUALITY
+
+        @items_qual_MIN = [
+          Item.new(name="+5 Dexterity Vest", sell_in=10, quality=min_quality),
+          Item.new(name="Aged Brie", sell_in=2, quality=min_quality),
+          Item.new(name="Elixir of the Mongoose", sell_in=5, quality=min_quality),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=min_quality),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=min_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=min_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=min_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=min_quality),
+          # This Conjured item does not work properly yet
+          # Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+        ]
+
+        @items_qual_MAX = [
+          Item.new(name="+5 Dexterity Vest", sell_in=10, quality=max_quality),
+          Item.new(name="Aged Brie", sell_in=2, quality=max_quality),
+          Item.new(name="Elixir of the Mongoose", sell_in=5, quality=max_quality),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=max_quality),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=max_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=max_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=max_quality),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=max_quality),
           # This Conjured item does not work properly yet
           # Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
         ]
       end
 
       it "quality is never negative" do
-        GildedRose.new(@items_qual_0).update_quality()
+        GildedRose.new(@items_qual_MIN).update_quality()
 
-        @items_qual_0.each do |item| 
+        @items_qual_MIN.each do |item| 
           expect(item.quality).to be >= 0
+        end
+      end
+
+      it "quality is never more than 50" do
+        GildedRose.new(@items_qual_MAX).update_quality()
+
+        @items_qual_MAX.each do |item| 
+          expect(item.quality).to be <= 50
         end
       end
     end
