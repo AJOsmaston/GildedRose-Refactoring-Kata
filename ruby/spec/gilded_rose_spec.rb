7b5381934441f4ca8e309cbfa7a36c2c69e9ef4a
AJOsmaston <dajoxx@gmail.com>
2021-11-24 16:48:48 +0000

    expanded quality test to all items (quality never negative)

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -6,6 +6,32 @@ describe GildedRose do
   #special items = aged brie, sulfuras, backstage passes
   #normal items = +5 vest, elixir
 
+    describe "all items" do
+
+      before(:each) do
+        @items_qual_0 = [
+          Item.new(name="+5 Dexterity Vest", sell_in=10, quality=0),
+          Item.new(name="Aged Brie", sell_in=2, quality=0),
+          Item.new(name="Elixir of the Mongoose", sell_in=5, quality=0),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=0),
+          Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=0),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=0),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=0),
+          Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=0),
+          # This Conjured item does not work properly yet
+          # Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+        ]
+      end
+
+      it "quality is never negative" do
+        GildedRose.new(@items_qual_0).update_quality()
+
+        @items_qual_0.each do |item| 
+          expect(item.quality).to be >= 0
+        end
+      end
+    end
+
     describe "normal items" do
       it "reduces quality by 1" do
         items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
@@ -26,13 +52,6 @@ describe GildedRose do
         GildedRose.new(items).update_quality()
 
         expect(items[0].quality).to eq 18
-      end
-
-      it "quality is never negative" do
-        items = [Item.new(name="+5 Dexterity Vest", sell_in=-1, quality=0)]
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].quality).to eq 0
       end
     end
   end
